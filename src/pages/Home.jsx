737fb38eb96db80e1134c178ccc722ae6ee6449d
asteriksdevteam <devteam@asteriksdigital.com>
2025-09-14 23:58:39 -0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../components/LatestCollection'
+
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <LatestCollection/>
+    </>
+  )
+}
+
+export default Home
