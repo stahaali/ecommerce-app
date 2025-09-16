8eabc30aa8dcd0e224606b91bcc33c620a665fe8
asteriksdevteam <devteam@asteriksdigital.com>
2025-09-16 00:53:52 -0700

    ok

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -26,7 +26,7 @@ const LatestCollection = () => {
         {latestProducts.map((item, index) => (
           <ProductItem
             key={index}
-            id={item.id}
+            id={item._id}
             image={item.image}
             name={item.name}
             price={item.price}
